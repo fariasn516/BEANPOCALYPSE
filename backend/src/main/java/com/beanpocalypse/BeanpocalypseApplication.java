da983c3b1f2b1264cec9586e634f297e160b8fc8
fariasn516 <145229665+fariasn516@users.noreply.github.com>
2025-02-08 09:56:16 -0500

    set-up basic backend

---
+++ backend/src/main/java/com/beanpocalypse/BeanpocalypseApplication.java
@@ -0,0 +1,12 @@
+package com.beanpocalypse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanpocalypseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BeanpocalypseApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/beanpocalypse/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanpocalypse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BeanpocalypseApplication.class)
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
